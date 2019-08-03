d6efc8a65e45175ec877a50810c5b6ffd311fb73
Jalen Adams <jalen@jalenkadams.me>
2019-08-03 09:19:31 -0400

    Add argument parsing

--- pyls.py
+++ pyls.py
@@ -1,11 +1,38 @@
 """ pyls - list directory contents
 
-Display information about the files in the specified directory.
+Display information about files in the specified directory.
 Uses the current directory by default.
 """
 
+import os
+import argparse
+
+def process_args():
+    parser = argparse.ArgumentParser(
+        prog="pyls",
+        description="Display information about files in the current directory." \
+            "\nUses the current directory by default.",
+        usage="%(prog)s [OPTION]... [FILE]...")
+
+    parser.add_argument(
+        "-a",
+        "--all",
+        action='store_true',
+        required=False,
+        help="display all files, including hidden ones")
+
+    parser.add_argument(
+        "files",
+        nargs='*',
+        default='.',
+        type=str,
+        metavar="FILE",
+        help="a space-separated list of files to display")
+
+    return parser.parse_args()
+
 def main():
-    print("Hello World!")
+    args = process_args()
 
 if __name__ == "__main__":
     main()
