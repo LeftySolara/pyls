99c0b387f03b8c1bfadb6260088bc4a8c9360da3
Jalen Adams <jalen@jalenkadams.me>
2019-08-03 10:38:55 -0400

    Get file info and print it

--- pyls.py
+++ pyls.py
@@ -4,10 +4,18 @@ Display information about files in the specified directory.
 Uses the current directory by default.
 """
 
-import os
 import argparse
+import os
+import shutil
+import textwrap
+from collections import namedtuple
+
+FileInfo = namedtuple("FileInfo", ["path", "name", "inode", "size", "mtime",
+                                   "mode", "hardlinks", "uid", "gid"])
 
 def process_args():
+    """Specify and parse command line arguments."""
+
     parser = argparse.ArgumentParser(
         prog="pyls",
         description="Display information about files in the current directory." \
@@ -31,8 +39,74 @@ def process_args():
 
     return parser.parse_args()
 
+
+def get_file_info(entry):
+    """Collect information for the given file.
+
+    Parameters
+    ----------
+    entry : DirEntry
+            The directory entry to collect information for.
+
+    Returns
+    -------
+    info : FileInfo
+           Named tuple containing file information.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the given file does not exist.
+
+    """
+
+    try:
+        stats = entry.stat()
+    except FileNotFoundError:
+        if entry.is_symlink(): # broken symlink
+            stats = entry.stat(follow_symlinks=False)
+        else:
+            raise FileNotFoundError
+
+    path = entry.path
+    name = entry.name
+    inode = stats.st_ino
+    size = stats.st_size
+    mtime = stats.st_mtime
+    mode = stats.st_mode
+    hardlinks = stats.st_nlink
+    uid = stats.st_uid
+    gid= stats.st_gid
+
+    info = FileInfo(path, name, inode, size, mtime,
+                    mode, hardlinks, uid, gid)
+
+    return info
+
+
 def main():
+    """Main entry point for the program."""
+
     args = process_args()
+    multiple_dirs = len(args.files) > 1
+    files = []
+
+    for file in args.files:
+        with os.scandir(file) as entries:
+            for entry in entries:
+                files.append(get_file_info(entry))
+
+        max_filename_len = len(max([file.name for file in files], key=len))
+        padded_names = [file.name.ljust(max_filename_len) for file in files]
+        column_count, row_count = shutil.get_terminal_size()
+
+        if multiple_dirs:
+            print(file + ':')
+        print(textwrap.fill("\n".join(padded_names), column_count))
+
+        if multiple_dirs:
+            print()
+
 
 if __name__ == "__main__":
     main()
