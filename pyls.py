dbf7c2f52eb53d5bfa002e115f969f4ab4a77222
Jalen Adams <jalen@jalenkadams.me>
2019-08-03 08:34:30 -0400

    Initial commit

---
+++ pyls.py
@@ -0,0 +1,11 @@
+""" pyls - list directory contents
+
+Display information about the files in the specified directory.
+Uses the current directory by default.
+"""
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
