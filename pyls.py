dfa6c2d1b84aec59b06b32a58cc46a3a2fd8966e
Jalen Adams <jalen@jalenkadams.me>
2019-08-04 12:00:10 -0400

    Migrate from os.Path to pathlib

--- pyls.py
+++ pyls.py
@@ -5,13 +5,10 @@ Uses the current directory by default.
 """
 
 import argparse
-import os
 import shutil
 import textwrap
-from collections import namedtuple
+import pathlib
 
-FileInfo = namedtuple("FileInfo", ["path", "name", "inode", "size", "mtime",
-                                   "mode", "hardlinks", "uid", "gid"])
 
 def process_args():
     """Specify and parse command line arguments.
@@ -48,82 +45,12 @@ def process_args():
     return namespace
 
 
-def get_dir_entries(dirname, include_hidden):
-    """Fetch a list of files/subdirectories in a directory.
-
-    Parameters
-    ----------
-    dirname : String
-        The name of the directory to search.
-
-    include_hidden : Bool
-        Whether to include files whose names start with '.'
-
-    Returns
-    -------
-    files : List
-        List of FileInfo objects with info about files in the given directory.
-
-    """
-
-    files = []
-    with os.scandir(dirname) as entries:
-        for entry in entries:
-            files.append(get_file_info(entry))
-
-    return files
-
-
-def get_file_info(entry):
-    """Collect information for the given file.
-
-    Parameters
-    ----------
-    entry : DirEntry
-        The directory entry to collect information for.
-
-    Returns
-    -------
-    info : FileInfo
-        Named tuple containing file information.
-
-    Raises
-    ------
-    FileNotFoundError
-        If the given file does not exist.
-
-    """
-
-    try:
-        stats = entry.stat()
-    except FileNotFoundError:
-        if entry.is_symlink(): # broken symlink
-            stats = entry.stat(follow_symlinks=False)
-        else:
-            print("Error: {}. No such file or directory.".format(entry.name))
-
-    path = entry.path
-    name = entry.name
-    inode = stats.st_ino
-    size = stats.st_size
-    mtime = stats.st_mtime
-    mode = stats.st_mode
-    hardlinks = stats.st_nlink
-    uid = stats.st_uid
-    gid = stats.st_gid
-
-    info = FileInfo(path, name, inode, size, mtime,
-                    mode, hardlinks, uid, gid)
-
-    return info
-
-
 def print_dir_contents(dir_path, print_hidden, print_header):
     """Display the contents of the given directory.
 
     Parameters
     ----------
-    dir_path : String
+    dir_path : pathlib.Path
         Path the to directory to print.
 
     print_hidden : Bool
@@ -134,16 +61,19 @@ def print_dir_contents(dir_path, print_hidden, print_header):
 
     """
 
-    files = get_dir_entries(dir_path, print_hidden)
-    max_filename_len = len(max([file.name for file in files], key=len))
-    padded_names = [file.name.ljust(max_filename_len) for file in files]
+    if not print_hidden:
+        paths = [child for child in dir_path.iterdir() if not child.name.startswith('.')]
+    else:
+        paths = [child for child in dir_path.iterdir()]
 
-    # row_count isn't needed for what we're doing here, but we have it because
-    # get_terminal_size() requires two things to assign its return values to.
+    max_filename_len = len(max([path.name for path in paths], key=len))
+    padded_names = [path.name.ljust(max_filename_len) for path in paths]
+
     column_count, row_count = shutil.get_terminal_size()
-
     if print_header:
-        print(dir_path + ':')
+        print()
+        print("{}:".format(dir_path))
+
     print(textwrap.fill("\n".join(padded_names), column_count))
 
 
@@ -154,13 +84,8 @@ def main():
     multiple_dirs = len(args.files) > 1
 
     for file in args.files:
-        try:
-            print_dir_contents(file, args.all, multiple_dirs)
-            if multiple_dirs:
-                print()
-
-        except FileNotFoundError:
-            print("{}: No such file or directory.\n".format(file))
+        current_directory = pathlib.Path(file)
+        print_dir_contents(current_directory, args.all, multiple_dirs)
 
 
 if __name__ == "__main__":
