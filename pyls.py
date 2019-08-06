d69f0953a1b838a6a135f8db3846f63307970424
Jalen Adams <jalen@jalenkadams.me>
2019-08-06 18:59:44 -0400

    Move most functionality into classes

--- pyls.py
+++ pyls.py
@@ -12,8 +12,7 @@ import pwd
 import shutil
 import stat
 import textwrap
-from collections import namedtuple
-from enum import Enum, unique
+from enum import Enum, unique, auto
 
 
 @unique
@@ -32,8 +31,148 @@ class Color(Enum):
     END = '\033[0m'
 
 
-FileInfo = namedtuple("FileInfo", ["filemode", "num_links", "uid", "gid",
-                                    "owner", "group", "size", "timestamp", "name"])
+@unique
+class FileType(Enum):
+    """Unix file types."""
+
+    DIR = 0        # Directory
+    REG = auto()   # Regular file
+    LNK = auto()   # Symbolic link
+    SOCK = auto()  # Socket
+    FIFO = auto()  # Named pipe
+    BLK = auto()   # Block device file
+    CHR = auto()   # Character special device file
+    UNKNOWN = auto()   # Unknown
+
+    # https://stackoverflow.com/questions/44595736/get-unix-file-type-with-python-os-module
+    @classmethod
+    def get_file_type(cls, path):
+        """Get the file type of the given path."""
+        if not isinstance(path, int):
+            path = path.lstat().st_mode
+        for path_type in cls:
+            method = getattr(stat, 'S_IS' + path_type.name.upper())
+            if method and method(path):
+                return path_type
+        return cls.UNKNOWN
+
+
+class FileInfo:
+    """An object in the filesystem."""
+
+    def __init__(self, path):
+        """Create a FileInfo instance and populate it with metadata.
+
+        Parameters
+        ----------
+        path : string
+            The path to the file. Can be relative or absolute.
+
+        """
+
+        self.path = pathlib.Path(path)
+        self.name = self.path.name
+        self.file_type = FileType.get_file_type(self.path)
+
+        self.filemode_str = None
+        self.num_links = None
+        self.size = None
+        self.mtime = None
+        self.uid = None
+        self.gid = None
+        self.owner = None
+        self.group = None
+
+    def __str__(self):
+        return self.name
+
+    def get_long_info(self):
+        """
+        Fetch information used to print in long format. If the file is a
+        symlink, get the info of the link rather than the target.
+
+        """
+
+        self.filemode_str = stat.filemode(self.path.lstat().st_mode)
+        self.num_links = self.path.lstat().st_nlink
+        self.size = self.path.lstat().st_size
+        self.mtime = self.path.lstat().st_mtime
+
+        # We have to get the user and group names from the uid and gid,
+        # since path.user() and path.group() get the info for the target
+        # rather than the symlink itself.
+        self.uid = self.path.lstat().st_uid
+        self.gid = self.path.lstat().st_gid
+        self.owner = pwd.getpwuid(self.uid)[0]
+        self.group = grp.getgrgid(self.gid)[0]
+
+    def get_long_str(self, link_padding=0, owner_padding=0,
+                     group_padding=0, size_padding=0):
+        """Generate a string for printing in long format.
+
+        Parameters
+        ----------
+        link_padding : int
+            Amount of padding to insert around link count.
+
+        owner_padding: int
+            Amount of padding to insert around owner name.
+
+        group_padding: int
+            Amount of padding to insert around group name.
+
+        size_padding=: int
+            Amount of padding to insert around the file size.
+
+        """
+
+        self.get_long_info()
+
+        name = self.name
+        link_padded = str(self.num_links).rjust(link_padding)
+        owner_padded = self.owner.rjust(owner_padding)
+        group_padded = self.group.rjust(group_padding)
+        size_padded = str(self.size).rjust(size_padding)
+
+        if self.file_type is FileType.DIR:
+            name = Color.BOLD.value + Color.BLUE.value + name + Color.END.value
+
+        if self.file_type is FileType.LNK:
+            name = "{} -> {}".format(name, self.path.resolve())
+
+        file_year = datetime.date.fromtimestamp(self.mtime).year
+        this_year = datetime.date.today().year
+        if file_year == this_year:
+            timestamp = datetime.datetime.fromtimestamp(self.mtime).strftime("%b %d %H:%M")
+        else:
+            timestamp = datetime.datetime.fromtimestamp(self.mtime).strftime("%b %d") + " " + str(file_year).rjust(5)
+
+        params = [self.filemode_str, link_padded, owner_padded, group_padded,
+                  size_padded, timestamp, name]
+
+        return " ".join(params)
+
+    def get_children(self, include_hidden=False):
+        """Get a list of files in this directory.
+
+        Parameters
+        ----------
+        include_hidden: Bool
+            Whether to include files whose names start with '.'
+        
+        Returns
+        -------
+        children : list
+            List of FileInfo objects.
+            
+        """
+
+        if not include_hidden:
+            children = [FileInfo(child) for child in self.path.iterdir() if not child.name.startswith('.')]
+        else:
+            children = [FileInfo(child) for child in self.path.iterdir()]
+        return children
+
 
 def process_args():
     """Specify and parse command line arguments.
@@ -77,144 +216,43 @@ def process_args():
     return namespace
 
 
-def get_file_info(file_path):
-    """Fetch information about a file.
-
-    Parameters
-    ----------
-    file_path : pathlib.Path
-        File to gather information for.
-
-    Returns
-    -------
-    info : FileInfo
-        Named tuple containing file information.
-
-    """
-
-    filemode = stat.filemode(file_path.lstat().st_mode)
-    num_hlinks = file_path.lstat().st_nlink
-    size = file_path.lstat().st_size
-    mtime = file_path.lstat().st_mtime
-
-    # We have to get the user and group names from the uid and gid,
-    # since path.user() and path.group() get the info for the target
-    # rather than the symlink itself.
-    uid = file_path.lstat().st_uid
-    gid = file_path.lstat().st_gid
-    owner = pwd.getpwuid(uid)[0]
-    group = grp.getgrgid(gid)[0]
-
-    if file_path.is_dir():
-        name = Color.BOLD.value + Color.BLUE.value + file_path.name + Color.END.value
-    else:
-        name = file_path.name
-
-    if file_path.is_symlink():
-        name = "{} -> {}".format(name, file_path.resolve())
-
-    file_year = datetime.date.fromtimestamp(mtime).year
-    if file_year == datetime.date.today().year:
-        timestamp = datetime.datetime.fromtimestamp(mtime).strftime("%b %d %H:%M")
-    else:
-        timestamp = datetime.datetime.fromtimestamp(mtime).strftime("%b %d") + " " + str(file_year).rjust(5)
-
-    info = FileInfo(filemode, num_hlinks, uid, gid, owner, group, size, timestamp, name)
-    return info
-
-
-def print_dir_contents(dir_path, print_hidden, print_header):
-    """Display the contents of the given directory.
-
-    Parameters
-    ----------
-    dir_path : pathlib.Path
-        Path the to directory to print.
-
-    print_hidden : Bool
-        Whether to include files whose names start with '.'
-
-    print_header : Bool
-        Whether to print the directory path as a header.
-
-    """
-
-    if not print_hidden:
-        paths = [child for child in dir_path.iterdir() if not child.name.startswith('.')]
-    else:
-        paths = [child for child in dir_path.iterdir()]
-
-    paths.sort(key=lambda path: path.name.lower().strip('.'))
-    max_filename_len = len(max([path.name for path in paths], key=len))
-    padded_names = [path.name.ljust(max_filename_len) for path in paths]
-
-    column_count, row_count = shutil.get_terminal_size()
-    if print_header:
-        print()
-        print("{}:".format(dir_path))
-
-    print(textwrap.fill("\n".join(padded_names), column_count))
-
-
-def print_dir_contents_long(dir_path, print_hidden, print_header):
-    """Display the contents of the given directory in long format.
-
-    Parameters
-    ----------
-    dir_path : pathlib.Path
-        Path the to directory to print.
-
-    print_hidden : Bool
-        Whether to include files whose names start with '.'
-
-    print_header : Bool
-        Whether to print the directory path as a header.
-
-    """
-
-    if not print_hidden:
-        files = [child for child in dir_path.iterdir() if not child.name.startswith('.')]
-    else:
-        files = [child for child in dir_path.iterdir()]
-    
-    files_info = []
-    for file in files:
-        files_info.append(get_file_info(file))
-    files_info.sort(key=lambda info: info.name.lower().strip('.'))
-
-    # Determine the amount of padding to use for each field.
-    max_links_width = len(max([str(info.num_links) for info in files_info], key=len))
-    max_owner_width = len(max([info.owner for info in files_info], key=len))
-    max_group_width = len(max([info.group for info in files_info], key=len))
-    max_size_width = len(max([str(info.size) for info in files_info], key=len))
-
-    padded_links = [str(info.num_links).rjust(max_links_width) for info in files_info]
-    padded_owners = [info.owner.rjust(max_owner_width) for info in files_info]
-    padded_goups = [info.group.rjust(max_group_width) for info in files_info]
-    padded_sizes = [str(info.size).rjust(max_size_width) for info in files_info]
-
-    for i in range(0, len(files_info)):
-        print(files_info[i].filemode, padded_links[i], padded_owners[i],
-              padded_goups[i], padded_sizes[i], files_info[i].timestamp, files_info[i].name)
-
-
 def main():
     """Main entry point for the program."""
 
     args = process_args()
-    multiple_dirs = len(args.files) > 1
+    # multiple_dirs = len(args.files) > 1
+    files = []
 
     for file in args.files:
-        current_path = pathlib.Path(file)
+        files.append(FileInfo(file))
 
-        if not current_path.is_dir():
-            print("\n{}".format(current_path))
-            continue
+    if args.long:
+        for file in files:
+            file.get_long_info()
 
-        if args.long:
-            print_dir_contents_long(current_path, args.all, multiple_dirs)
-        else:
-            print_dir_contents(current_path, args.all, multiple_dirs)
+    max_links_width = len(max([str(file.num_links) for file in files], key=len))
+    max_owner_width = len(max([file.owner for file in files], key=len))
+    max_group_width = len(max([file.group for file in files], key=len))
+    max_size_width = len(max([str(file.size) for file in files], key=len))
+
+    column_count, row_count = shutil.get_terminal_size()
+
+    if args.long:
+        for file in files:
+            children = file.get_children(args.all)
+            children.sort(key=lambda child: child.name.lower().strip('.'))
+
+            for child in children:
+                out_str = child.get_long_str(max_links_width, max_owner_width,
+                                             max_group_width, max_size_width)
+                print(out_str)
+    else:
+        for file in files:
+            children = file.get_children(args.all)
+            children.sort(key=lambda child: child.name.lower().strip('.'))
+            max_filename_len = len(max([child.name for child in children], key=len))
+            padded_names = [child.name.ljust(max_filename_len) for child in children]
+            print(textwrap.fill("\n".join(padded_names), column_count))
 
 
 if __name__ == "__main__":
